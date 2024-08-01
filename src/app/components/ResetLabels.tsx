7957eb8bb932a64098f004bb30d23991bd99d96a
Tomas <tmercadoslp@gmail.com>
2024-08-01 09:31:49 -0300

    feat(components): add ResetLabels

---
+++ src/app/components/ResetLabels.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+import { RefreshCcw } from "lucide-react";
+import { useState } from "react";
+import { Alert } from "./Alert";
+
+export const ResetLabels = ({ resetLabels }: { resetLabels: () => void }) => {
+  const [isAlertOpen, setIsAlertOpen] = useState(false);
+
+  return (
+    <>
+      <Button
+        size="icon"
+        className="size-6"
+        onClick={() => {
+          setIsAlertOpen(true);
+        }}
+      >
+        <RefreshCcw className="size-4" />
+      </Button>
+      <Alert
+        onOpenChange={setIsAlertOpen}
+        open={isAlertOpen}
+        title="Remove all labels"
+        description="Are you sure you want to remove all labels?"
+        leftButton={{
+          label: "Cancel",
+          onClick: () => setIsAlertOpen(false),
+        }}
+        rightButton={{
+          label: "Remove",
+          onClick: resetLabels,
+        }}
+      />
+    </>
+  );
+};
