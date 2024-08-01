65e31903dc8eae6cec2010e8b7da21b44b344a65
Tomas <tmercadoslp@gmail.com>
2024-07-31 21:45:36 -0300

    feat(auth): add accessToken to session

--- src/app/lib/auth/index.ts
+++ src/app/lib/auth/index.ts
@@ -16,6 +16,21 @@ const issuer = process.env.GITLAB_AUTH_URL;
 
 export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth({
   secret: process.env.SECRET,
+  callbacks: {
+    jwt: ({ token, account }) => {
+      if (account?.access_token) {
+        token.accessToken = account.access_token
+      }
+      return token;
+    },
+    session: ({ session, token }) => {
+      if (typeof token.accessToken !== 'string') {
+        throw new Error('accessToken is not a string');
+      }
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
   providers: [
     {
       id: 'gitlab',
@@ -23,6 +38,10 @@ export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth({
       type: 'oidc',
       issuer,
       token: `${issuer}/oauth/token`,
+      authorization: {
+        url: `${issuer}/oauth/authorize`,
+        params: { scope: 'openid api' },
+      },
       clientId: process.env.GITLAB_CLIENT_ID,
       clientSecret: process.env.GITLAB_CLIENT_SECRET,
       userinfo: `${issuer}/api/v4/user`,
