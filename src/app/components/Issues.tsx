bbdf680568282670f3bb9e1c6589fb4dc88542df
Tomas <tmercadoslp@gmail.com>
2024-08-02 14:37:59 -0300

    feat: implement TwoResizableColumns

--- src/app/components/Issues.tsx
+++ src/app/components/Issues.tsx
@@ -6,45 +6,34 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
-import { InputsRow } from "./InputsRow";
 import { Issue, IssueItem } from "./IssueItem";
-import { LabelOption } from "./SelectLabels";
 
 export interface IssuesProps {
-  labels: LabelOption[];
   issues: Issue[];
-  onRemoveLabel: (label: string) => void;
-  milestoneForm: React.ReactNode;
+  header: React.ReactNode;
 }
 
-export function Issues({
-  labels,
-  issues,
-  milestoneForm,
-  onRemoveLabel,
-}: IssuesProps) {
+export function Issues({ issues, header }: IssuesProps) {
   return (
-    <div className="border overflow-hidden w-full bg-background-secondary px-md2 py-md3 rounded-lg">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[250px]">Title</TableHead>
-            <TableHead>Labels</TableHead>
-            <TableHead>Milestone</TableHead>
-            <TableHead className="w-16">Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <InputsRow
-            labelOptions={labels}
-            milestoneForm={milestoneForm}
-            onRemoveLabel={onRemoveLabel}
-          />
-          {issues.map((issue) => (
-            <IssueItem key={issue.id} {...issue} />
-          ))}
-        </TableBody>
-      </Table>
+    <div className="border overflow-scroll h-full w-full bg-background-secondary px-md2 py-md3 rounded-lg flex flex-col gap-md1">
+      {header}
+      <div className="max-h-[480px] h-full overflow-auto">
+        <Table className="w-full">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="min-w-48">Title</TableHead>
+              <TableHead>Labels</TableHead>
+              <TableHead className="min-w-32">Milestone</TableHead>
+              <TableHead>Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {issues.map((issue) => (
+              <IssueItem key={issue.id} {...issue} />
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
--- src/app/page.client.tsx
+++ src/app/page.client.tsx
@@ -1,52 +1,47 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { getRandomHex, getTextColor } from "@/lib/colors";
-import { Bot, Tag, Trash2, Upload } from "lucide-react";
-import { useState } from "react";
+import { Bot, CloudDownload, Trash2, Upload } from "lucide-react";
 import { AddIssuesDialog } from "./components/AddIssuesDialog";
-import { AddLabelForm } from "./components/AddLabelForm";
 import { EditModalDialog } from "./components/EditModalDialog";
 import { ImportLabelsDialog } from "./components/ImportLabelsDialog";
 import { InferDialog } from "./components/InferDialog";
-import { Issue } from "./components/IssueItem";
+import { IssueForm, parseIssueFormResult } from "./components/IssueForm";
 import { Issues } from "./components/Issues";
-import { MilestoneForm } from "./components/MilestoneForm";
 import { ResetLabels } from "./components/ResetLabels";
+import { TwoResizableColumns } from "./components/TwoResizableColumns";
 import { useIssues } from "./hooks/useIssues";
-import { useLabels } from "./hooks/useLabels";
+import { LabelOption, generateLabelOption, useLabels } from "./hooks/useLabels";
+
+const combineEditingLabelsWithLabels = (
+  editingLabels: LabelOption[],
+  labels: LabelOption[]
+) => {
+  const labelsNotRepeated = [
+    ...labels.map((label) => label.label),
+    ...editingLabels.map((l) => l.label),
+  ].filter(Boolean);
+
+  const uniqueLabels = Array.from(new Set(labelsNotRepeated));
+
+  return uniqueLabels.map((label) => {
+    const existingLabel = labels.find((l) => l.label === label);
+
+    if (!existingLabel) {
+      return generateLabelOption({ label });
+    }
+
+    return existingLabel;
+  });
+};
 
 export default function ClientPage() {
   const [labels, addLabel, removeLabel, resetLabels] = useLabels();
   const [issues, addIssue, clearIssues, { editing, setEditing }, removeIssue] =
     useIssues();
-  const [milestone, setMilestone] = useState<string | null>(null);
 
   return (
     <>
-      <div className="flex justify-between w-full">
-        <div className="flex flex-col gap-sm1 rounded-lg min-w-80 p-4 border border-muted">
-          <div className="flex w-full justify-between gap-4">
-            <p className="font-semibold">Labels</p>
-
-            <div className="flex gap-2">
-              <ResetLabels resetLabels={resetLabels} />
-              <ImportLabelsDialog
-                trigger={
-                  <Button size="icon" className="size-6">
-                    <Tag className="size-4" />
-                  </Button>
-                }
-                onSuccess={(payload) => {
-                  resetLabels();
-                  payload.forEach(({ name, color }) => addLabel(name, color));
-                }}
-              />
-            </div>
-          </div>
-
-          <AddLabelForm addLabel={addLabel} />
-        </div>
-
+      <div className="flex justify-end w-full">
         <div className="flex gap-4">
           <AddIssuesDialog
             trigger={
@@ -63,8 +58,7 @@ export default function ClientPage() {
                 <Bot className="mr-2 h-4 w-4" /> AI Infer
               </Button>
             }
-            labels={labels.map((label) => label.label)}
-            milestone={milestone ?? ""}
+            labels={labels}
             onSuccess={(newIssues) => {
               clearIssues();
               newIssues.forEach(addIssue);
@@ -73,84 +67,74 @@ export default function ClientPage() {
         </div>
       </div>
 
-      <div className="col-span-8">
-        <div>
-          <div className="flex justify-between w-full">
-            <h2 className="text-2xl font-bold mb-4">Issues</h2>
-
-            <Button variant="secondary" onClick={clearIssues}>
-              <Trash2 className="mr-2 h-4 w-4" /> Clear Issues
-            </Button>
-          </div>
+      <TwoResizableColumns
+        columnA={
+          <form
+            action={(data: FormData) => {
+              addIssue(parseIssueFormResult(data, labels));
+            }}
+            className="h-full flex flex-col gap-md1 px-md2 py-md3 rounded-lg bg-background-secondary border"
+          >
+            <p className="text-h4 font-semibold">Add issue</p>
+            <IssueForm
+              labelOptions={labels}
+              labelsActions={
+                <div className="flex gap-2">
+                  <ResetLabels resetLabels={resetLabels} />
+                  <ImportLabelsDialog
+                    trigger={
+                      <Button size="icon" className="size-6">
+                        <CloudDownload className="size-4" />
+                      </Button>
+                    }
+                    onSuccess={(payload) => {
+                      resetLabels();
+                      payload.forEach(({ name, color }) =>
+                        addLabel(name, color)
+                      );
+                    }}
+                  />
+                </div>
+              }
+              onRemoveLabel={removeLabel}
+            />
+          </form>
+        }
+        columnB={
           <Issues
-            labels={labels}
-            issues={issues}
-            milestoneForm={
-              <MilestoneForm
-                milestone={milestone}
-                setMilestone={setMilestone}
-              />
-            }
-            onRemoveLabel={removeLabel}
-          />
-        </div>
-        <form
-          id="issue-form"
-          action={(data: FormData) => {
-            const labels = data.get("label");
+            header={
+              <div className="flex justify-between w-full">
+                <p className="text-h4 font-semibold mb-4">Issues</p>
 
-            addIssue({
-              title: data.get("title") as string,
-              labels:
-                labels && typeof labels === "string" ? labels.split(",") : [],
-              description: data.get("description") as string,
-              milestone: data.get("milestone") as string,
-            });
-          }}
-        >
-          <input hidden readOnly name="milestone" value={milestone ?? ""} />
-        </form>
-      </div>
+                <Button variant="secondary" onClick={clearIssues}>
+                  <Trash2 className="mr-2 h-4 w-4" /> Clear Issues
+                </Button>
+              </div>
+            }
+            issues={issues}
+          />
+        }
+      />
+
+      <form
+        action={(data: FormData) => {
+          addLabel(data.get("label") as string);
+        }}
+        className="flex flex-col gap-2"
+        id="add-label"
+      />
 
       <EditModalDialog
         editing={editing}
-        onAddLabel={(label) => {
-          setEditing({
-            ...editing,
-            labels: [...(editing?.labels ?? []), label],
-          } as Issue);
-        }}
         onClose={() => setEditing(null)}
-        onRemoveLabel={(label) => {
-          setEditing({
-            ...editing,
-            labels: editing?.labels.filter((l) => l !== label) ?? [],
-          } as Issue);
-        }}
         onUpdate={(issue) => {
           removeIssue(issue.id);
           addIssue(issue);
           setEditing(null);
         }}
-        labelOptions={Array.from(
-          new Set(
-            [...labels.map((label) => label.label), ...(editing?.labels ?? [])]
-              .filter(Boolean)
-              .map((label) => {
-                const existingLabel = labels.find((l) => l.label === label);
-
-                if (!existingLabel) {
-                  const backgroundColor = getRandomHex();
-                  return {
-                    label,
-                    backgroundColor,
-                    color: getTextColor(backgroundColor),
-                  };
-                }
-
-                return existingLabel;
-              })
-          )
+        labelOptions={combineEditingLabelsWithLabels(
+          editing?.labels ?? [],
+          labels
         )}
       />
     </>
